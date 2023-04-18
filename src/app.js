e3991055c2d77cba49770d710e31d20fc6ff7d96
Matheus <matheusdemedeiros200599@gmail.com>
2023-04-18 02:00:15 -0300

    feat: cria controller e routes para livro

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import books from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on('error', console.log.bind(console, 'Erro de conexão'))
 db.once('open', () => {
@@ -10,20 +11,7 @@ db.once('open', () => {
 const app = express();
 app.use(express.json());
 
-// const books = [
-//   {id: 1, "title": "Senhor dos Aneis"},
-//   {id: 2, "title": "O Hobbit"}
-// ];
-
-app.get('/', (req, res) => {
-  res.status(200).send('Curso de Node');
-});
-
-app.get('/livros', (req, res) => {
-  books.find((err, books) => {
-    res.status(200).json(books);
-  })
-});
+routes(app);
 
 app.get('/livros/:id', (req, res) => {
   let index = getBook(req.params.id);
